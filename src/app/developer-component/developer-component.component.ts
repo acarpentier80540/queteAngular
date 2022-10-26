fd82197b08bb2f5036a1f456c8e00d7cda138b2f
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-10-26 09:59:33 +0200

    quete7

--- src/app/developer-component/developer-component.component.ts
+++ src/app/developer-component/developer-component.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { developer } from '../models/developer.model';
+import { skill } from '../models/skill.model';
 @Component({
   selector: 'app-developer-component',
   templateUrl: './developer-component.component.html',
   styleUrls: ['./developer-component.component.css']
 })
 export class DeveloperComponentComponent implements OnInit {
-
+  developer: developer = new developer('Carpentier', 'Antoine', 26, 'Homme', 'J aime bien la biere', [
+    new skill('Goudale', 'G', 'Goudale.com'),
+    new skill('Leffe', 'L', 'leffe.com'),
+    
+  ]);
   constructor() { }
 
   ngOnInit(): void {
